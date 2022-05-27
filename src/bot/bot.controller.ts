aa429ecda7b64c505b1bc9f24befabd50f93cc6c
Saeed <sa.ghofraniivari@gmail.com>
2022-05-27 15:44:11 +0430

    expose code

--- src/bot/bot.controller.ts
+++ src/bot/bot.controller.ts
@@ -10,12 +10,13 @@ export class BotController {
 
     @Get()
     async Bot(): Promise<object> {
-        
-        this.userService.startBot('5380617566:AAGNYy2DvzbMRTdOtJazVhPv8I6dEclKTiI');
+
+        const code = await this.userService.startBot('5380617566:AAGNYy2DvzbMRTdOtJazVhPv8I6dEclKTiI');
 
         const objJson1 = JSON.parse(JSON.stringify({
             botStarted: true,
-            botId: '@test_1382_bot'
+            botId: '@test_1382_bot',
+            code: code
         }));
         return objJson1;
     }
