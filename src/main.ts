d8ddb8a15c1f336be50325074efebd7473514800
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2022-05-29 12:26:17 +0430

    add nestjs pino

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  const app: NestExpressApplication = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   const config: ConfigService = app.get(ConfigService);
   const port: number = config.get<number>('PORT');
-
+  app.useLogger(app.get(Logger));
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Telegraf Bot')
     .setDescription('The Bot API description')
