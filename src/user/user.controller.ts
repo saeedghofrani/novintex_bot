5c25609a6a6f4d947cafdc61f29307292a02a2f5
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2022-05-29 14:15:38 +0430

    refactor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,7 +4,7 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
   async createUser(@Body() body: CreateUserDto) {
@@ -36,5 +36,5 @@ export class UserController {
   async findChildTree(@Body() body: any) {
     return this.userService.findChildTree(body.secondId);
   }
-  
+
 }
